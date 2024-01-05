ec20f7e7805d85df05205c1c81e82e34683a2655
Gustavo Canedo <gknedo@gmail.com>
2024-01-04 23:38:40 -0300

    Updates

---
+++ precache-manifest.7f8af6905f3a565967822cdca135616d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17627eddf6eb2b3e95f5972d8587ffc7",
+    "url": "/hand-history/index.html"
+  },
+  {
+    "revision": "a304fe939f35aa56aa03",
+    "url": "/hand-history/static/css/main.290d4c80.chunk.css"
+  },
+  {
+    "revision": "dd2afa6f510ce0634113",
+    "url": "/hand-history/static/js/2.08ec22dc.chunk.js"
+  },
+  {
+    "revision": "1bcdc795fbd812d7fcc5",
+    "url": "/hand-history/static/js/3.110ad6b2.chunk.js"
+  },
+  {
+    "revision": "a304fe939f35aa56aa03",
+    "url": "/hand-history/static/js/main.66e04de5.chunk.js"
+  },
+  {
+    "revision": "10d7af58bdde231d3671",
+    "url": "/hand-history/static/js/runtime~main.e7003f3c.js"
+  }
+]);
